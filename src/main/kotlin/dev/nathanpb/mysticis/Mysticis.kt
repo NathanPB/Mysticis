54d3161e767ed2145f74f5e999ad1aa4eb5365f3
NathanPB <nathan.pbombana@gmail.com>
2020-01-11 15:31:11 -0300

    Setting up workspace

--- src/main/kotlin/dev/nathanpb/mysticis/Mysticis.kt
+++ src/main/kotlin/dev/nathanpb/mysticis/Mysticis.kt
@@ -1,9 +1,15 @@
-![Mysticis Logo](src/main/resources/assets/mysticis/icon.png)
+package dev.nathanpb.mysticis
 
-# License
-
+/*
 Copyright (C) 2019 Nathan P. Bombana
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+*/
+
+@Suppress("unused")
+fun init() {
+    println("Hello Fabric world!")
+}
+
