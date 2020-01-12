9d9c4c190a58225276bef5916da32e0cd7931e98
NathanPB <nathan.pbombana@gmail.com>
2020-01-12 02:36:36 -0300

    Mana regen based on the affinity

--- src/main/kotlin/dev/nathanpb/mysticis/listener/ManaRegenListener.kt
+++ src/main/kotlin/dev/nathanpb/mysticis/listener/ManaRegenListener.kt
@@ -1,8 +1,10 @@
-package dev.nathanpb.mysticis
+package dev.nathanpb.mysticis.listener
 
+import dev.nathanpb.mysticis.data.mana
+import dev.nathanpb.mysticis.data.manaAffinity
 import dev.nathanpb.mysticis.event.entity.PlayerTickCallback
-import dev.nathanpb.mysticis.listener.AffinityListener
-import dev.nathanpb.mysticis.listener.ManaRegenListener
+import net.minecraft.entity.player.PlayerEntity
+
 
 /*
 Copyright (C) 2019 Nathan P. Bombana
@@ -11,10 +13,12 @@ This program is free software: you can redistribute it and/or modify it under th
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
 */
-
-@Suppress("unused")
-fun init() {
-    PlayerTickCallback.EVENT.register(AffinityListener())
-    PlayerTickCallback.EVENT.register(ManaRegenListener())
+class ManaRegenListener : PlayerTickCallback {
+    override fun tick(player: PlayerEntity?) {
+        if(player?.world?.isClient == false) {
+            player.mana += player.manaAffinity.mapValues { value, _ ->
+                value / 1000
+            }
+        }
+    }
 }
-
