63ad1ead7c6f51c05c12b531d64be25e33b7a149
NathanPB <nathan.pbombana@gmail.com>
2020-01-22 05:41:18 -0300

    Finally some fucking crafting system owo

--- src/main/kotlin/dev/nathanpb/mysticis/containers/WandAssemblerContainer.kt
+++ src/main/kotlin/dev/nathanpb/mysticis/containers/WandAssemblerContainer.kt
@@ -4,12 +4,22 @@ import dev.nathanpb.mysticis.blocks.entity.WandAssemblerEntity
 import dev.nathanpb.mysticis.items.staff.IStaffCrystal
 import dev.nathanpb.mysticis.items.staff.IStaffHead
 import dev.nathanpb.mysticis.items.staff.IStaffRod
+import dev.nathanpb.mysticis.recipe.STAFF_ASSEMBLER
 import dev.nathanpb.mysticis.utils.InputRestrictedSlot
 import dev.nathanpb.mysticis.utils.OutputSlot
+import net.minecraft.client.network.packet.GuiSlotUpdateS2CPacket
 import net.minecraft.container.Container
+import net.minecraft.container.ContainerListener
+import net.minecraft.container.CraftingContainer
 import net.minecraft.container.Slot
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.inventory.Inventory
+import net.minecraft.item.ItemStack
+import net.minecraft.recipe.Recipe
+import net.minecraft.recipe.RecipeFinder
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.DefaultedList
 
 
 /*
@@ -23,7 +33,7 @@ class WandAssemblerContainer(
     syncId: Int,
     val playerInventory: PlayerInventory,
     private val blockEntity: WandAssemblerEntity
-) : Container(null, syncId) {
+) : CraftingContainer<Inventory>(null, syncId) {
 
     init {
         addSlot(InputRestrictedSlot(blockEntity, 0, 56, 6) {
@@ -36,7 +46,16 @@ class WandAssemblerContainer(
             it?.item is IStaffCrystal
         })
 
-        addSlot(OutputSlot(blockEntity, 3, 116, 26))
+        addSlot(object: OutputSlot(blockEntity, 3, 116, 26) {
+            override fun onTakeItem(playerEntity_1: PlayerEntity?, itemStack_1: ItemStack?): ItemStack? {
+                (0..2).forEach {
+                    this@WandAssemblerContainer.slotList[it]?.let { slot ->
+                        slot.stack.decrement(1)
+                    }
+                }
+                return super.onTakeItem(playerEntity_1, itemStack_1)
+            }
+        })
 
         (0..2).forEach { i ->
             (0..8).forEach { m ->
@@ -51,7 +70,61 @@ class WandAssemblerContainer(
         (0..8).forEach { i ->
             addSlot(Slot(playerInventory, i, 8 + i * 18, 142))
         }
+
+        addListener(object: ContainerListener {
+            override fun onContainerRegistered(container: Container?, defaultedList: DefaultedList<ItemStack>?) {
+                // Not Implemented
+            }
+
+            override fun onContainerPropertyUpdate(container: Container?, propertyId: Int, i: Int) {
+                // Not Implemented
+            }
+
+            override fun onContainerSlotUpdate(container: Container?, slotId: Int, itemStack: ItemStack?) {
+                if (container == this@WandAssemblerContainer && slotId <= 3) {
+                   this@WandAssemblerContainer.onContentChanged(blockEntity)
+                }
+            }
+        })
+    }
+
+    override fun onContentChanged(inventory: Inventory?) {
+        super.onContentChanged(inventory)
+        blockEntity.markDirty()
+
+        if (blockEntity.world?.isClient == false) {
+            blockEntity.world?.server?.let { server ->
+                val stack = server.recipeManager
+                    .getFirstMatch(STAFF_ASSEMBLER, blockEntity, blockEntity.world)
+                    .orElse(null)?.craft(blockEntity) ?: ItemStack.EMPTY
+
+                getSlot(craftingResultSlotIndex).stack = stack
+                (playerInventory.player as ServerPlayerEntity)
+                    .networkHandler
+                    .sendPacket(GuiSlotUpdateS2CPacket(syncId, craftingResultSlotIndex, stack))
+            }
+        }
     }
 
     override fun canUse(player: PlayerEntity?) = blockEntity.canPlayerUseInv(player)
+
+    override fun populateRecipeFinder(recipeFinder: RecipeFinder?) {
+        recipeFinder?.let {
+            this.blockEntity.items.forEach(recipeFinder::addItem)
+        }
+    }
+
+    override fun getCraftingResultSlotIndex() = 3
+
+    override fun getCraftingWidth() = 1
+
+    override fun getCraftingHeight() = 3
+
+    override fun matches(recipe: Recipe<in Inventory>?) = recipe?.matches(blockEntity, blockEntity.world) == true
+
+    override fun getCraftingSlotCount() = 3
+
+    override fun clearCraftingSlots() {
+        blockEntity.clear()
+    }
 }
