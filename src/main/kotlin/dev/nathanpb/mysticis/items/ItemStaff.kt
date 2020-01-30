NathanPB/Mysticis

commit fa051bbf5cfc601b9f1830191623c88b5d1e6c9f

Made the Single Use thing work again

src/main/kotlin/dev/nathanpb/mysticis/items/ItemStaff.kt
@@ -99,7 +99,32 @@ class ItemStaff : RangedWeaponItem(Settings().maxCount(1).group(CREATIVE_TAB)) {
     }
 
     override fun use(world: World?, user: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
-        user?.setCurrentHand(Hand.MAIN_HAND)
+        user?.getStackInHand(hand)?.let { stack ->
+            stack.staffData.crystal?.item?.let { crystalItem ->
+                if (crystalItem is ISingleUseStaffCrystal) {
+                    val cost = if (user.isCreative) {
+                        ManaData()
+                    } else {
+                        crystalItem.singleUseCost(user, stack, null, null)
+                    }
+
+                    val oldMana = user.mana
+                    val newMana = oldMana - cost
+
+                    if (!newMana.hasNegatives()) {
+                        val result = crystalItem.onSingleUse(user, stack, null, null).result
+
+                        if (result == ActionResult.CONSUME  && newMana != oldMana) {
+                            user.mana = newMana
+                            ManaChangedCallback.EVENT
+                                .invoker()
+                                .onManaChanged(user, newMana, oldMana, ManaChangedCause.USED_BY_STAFF)
+                        }
+                    }
+                }
+            }
+        }
+
         return super.use(world, user, hand)
     }
 

commit 62c9876a11932e7a8d37b912b42a7f72aa71c56f

Fixed staff continuous usage not working anymore, broke since fa051bbf5cfc601b9f1830191623c88b5d1e6c9f

src/main/kotlin/dev/nathanpb/mysticis/items/ItemStaff.kt
@@ -99,6 +99,7 @@ class ItemStaff : RangedWeaponItem(Settings().maxCount(1).group(CREATIVE_TAB)) {
     }
 
     override fun use(world: World?, user: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
+        user?.setCurrentHand(hand)
         user?.getStackInHand(hand)?.let { stack ->
             stack.staffData.crystal?.item?.let { crystalItem ->
                 if (crystalItem is ISingleUseStaffCrystal) {